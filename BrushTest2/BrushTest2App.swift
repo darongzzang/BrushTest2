0071b2a012491646c4cc6c69a6aee97fdb2a593e
darongzzang <101727090+darongzzang@users.noreply.github.com>
2024-09-25 20:32:10 +0900

    Initial Commit

---
+++ BrushTest2/BrushTest2App.swift
@@ -0,0 +1,17 @@
+//
+//  BrushTest2App.swift
+//  BrushTest2
+//
+//  Created by 김이예은 on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct BrushTest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
